fe6ab32ad72bdffe10054b7477cd8c53b2efe072
Nchovati <102630174+Nchovati@users.noreply.github.com>
2022-06-23 22:59:03 -0400

    Header added

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img
+        className="headerImg"
+        src="https://static3.depositphotos.com/1000684/109/i/950/depositphotos_1098153-stock-photo-breaking-news.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
